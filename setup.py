859232c032b79ec031ae09f2d6b3ddaf190fa852
snkas <snkasdev@gmail.com>
2021-05-12 15:49:54 +0200

    v1.8.1

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ from distutils.core import setup
 
 setup(
     name='exputil',
-    version='1.8.0',
+    version='1.8.1',
     description='Experiment utilities',
     license='MIT',
     python_requires='>=3.7',
